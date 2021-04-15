29eeabc94b274e911949543f1a28131aea77d22e
inv <luis.lhsn@gmail.com>
2021-04-14 22:02:58 -0300

    Alteracoes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function (){
+    return view('site.sobre');
+});
+
+Route::get('/tecnologias', function () {
+    return view('site.tecnologias');
+});
+
+Route::get('/projetos', function () {
+    return view('projetos');
+});
+
+Route::get('/contatos', function () {
+    return view('contatos');
+});
